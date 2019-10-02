a9476a811067f68a887b3fcf17528c3781cfb917
Saleem Latif <saleem.latif.ee@gmail.com>
2019-10-02 18:39:32 +0500

    Version upgrade.

--- theming/__init__.py
+++ theming/__init__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 
 from .core import get_base_dir, get_themes, is_enabled
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 
 default_app_config = "theming.apps.ThemingConfig"  # pylint: disable=invalid-name
